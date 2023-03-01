e8f3ed68d4eadb759480eae10a4d08caa36eaac2
Arti mapari <artimapari6@gmail.com>
2023-03-01 20:06:54 +0530

    First commit

---
+++ src/org/ATM/mysqlconnetor.java
@@ -0,0 +1,36 @@
+package org.ATM;
+
+
+import java.sql.*;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+
+public class mysqlconnetor {
+  
+  static Connection conn=null;
+  
+  public static Connection getConnection() throws SQLException, ClassNotFoundException
+  {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "Artimapari6@");
+      return conn;
+  }
+  
+  
+  public static void closeConnection()
+  {
+      if(conn!=null)
+      {
+          try 
+          {
+              conn.close();
+          } 
+          catch (SQLException e) 
+          {
+              
+              System.out.println("Something went wrong!!");
+          }
+      }
+  }
+
+}
